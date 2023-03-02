d11ed3ceae555d61dfc20605aa8922583a463130
FaizulHassan-Rhine <faizulhassan.rien@gmail.com>
2023-03-02 13:02:29 +0600

    chnage title and logo of url

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
-import { DatePicker } from "antd";
-import { FaBeer } from "react-icons/fa";
-
 function App() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold  underline">Hello world!</h1>
-      <DatePicker />;
-      <h3>
-        {" "}
-        Lets go for a <FaBeer />?{" "}
-      </h3>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
