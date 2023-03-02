cb07061b5d82eb8d2373d95cbd0f07a6975b8ec4
FaizulHassan-Rhine <faizulhassan.rien@gmail.com>
2023-03-02 12:54:32 +0600

    install react icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { DatePicker } from "antd";
+import { FaBeer } from "react-icons/fa";
 
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold  underline">Hello world!</h1>
       <DatePicker />;
+      <h3>
+        {" "}
+        Lets go for a <FaBeer />?{" "}
+      </h3>
     </div>
   );
 }
