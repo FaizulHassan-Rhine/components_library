ad362992027b8f45c6247b01956fec75c3293fb8
FaizulHassan-Rhine <faizulhassan.rien@gmail.com>
2023-03-02 12:52:09 +0600

    install antDesign

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { DatePicker } from "antd";
+
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold  underline">Hello world!</h1>
+      <DatePicker />;
     </div>
   );
 }
