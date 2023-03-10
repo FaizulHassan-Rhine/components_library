cc6d6c709718b0a9dc2105f8928a332cf48e1c2b
FaizulHassan-Rhine <faizulhassan.rien@gmail.com>
2023-03-10 12:13:55 +0600

    add VideoViewer component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 // import Button from "./Components/Button/Button";
 import Home from "./Components/Home/Home";
 import ImageViewer from "./Components/ImageViewer/ImageViewer";
+import VideoViewer from "./Components/VideoViewer/VideoViewer";
 
 function App() {
   return (
     <div>
       <Home></Home>
       <ImageViewer></ImageViewer>
+      <VideoViewer></VideoViewer>
       {/* <Button></Button> */}
     </div>
   );
---
+++ src/Components/VideoViewer/VideoViewer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import video from "./video/production.mp4";
+
+const VideoViewer = () => {
+  return (
+    <div className="flex container mx-auto justify-center">
+      <video className="mt-20 mb-20" width="90%" height="450" controls>
+        <source src={video} type="video/mp4" />
+      </video>
+    </div>
+  );
+};
+
+export default VideoViewer;
