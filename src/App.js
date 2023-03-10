09fa7db2ab475f0e40bfc4ed3056393a086c9c0d
FaizulHassan-Rhine <faizulhassan.rien@gmail.com>
2023-03-10 12:01:02 +0600

    add photoView component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 // import Button from "./Components/Button/Button";
 import Home from "./Components/Home/Home";
+import ImageViewer from "./Components/ImageViewer/ImageViewer";
 
 function App() {
   return (
     <div>
       <Home></Home>
+      <ImageViewer></ImageViewer>
       {/* <Button></Button> */}
     </div>
   );
