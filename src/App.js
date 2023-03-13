e40f6bad26f987c7bf079c71aaa502ade4ad0263
FaizulHassan-Rhine <faizulhassan.rien@gmail.com>
2023-03-13 13:23:12 +0600

    chnage some structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-// import Button from "./Components/Button/Button";
+import Button from "./Components/Button/Button";
 import Home from "./Components/Home/Home";
 import ImageViewer from "./Components/ImageViewer/ImageViewer";
 import VideoViewer from "./Components/VideoViewer/VideoViewer";
+import ProductColorChange from "./Components/ProductColorChange/ServiceExplore";
 
 function App() {
   return (
     <div>
-      <Home></Home>
-      <ImageViewer></ImageViewer>
-      <VideoViewer></VideoViewer>
-      {/* <Button></Button> */}
+      {/* <Home></Home> */}
+      {/* <ImageViewer></ImageViewer> */}
+      {/* <VideoViewer></VideoViewer> */}
+      {/* <ProductColorChange></ProductColorChange> */}
+      <Button></Button>
     </div>
   );
 }
