8a4c2df0f1c1b1658d11efc9080498745d99a920
faizulhassanRien <faizulhassan.rien@gmail.com>
2024-05-28 16:08:17 +0600

    add download option

--- src/Components/ImageSlider/ImageSlider.js
+++ src/Components/ImageSlider/ImageSlider.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import ReactCompareImage from 'react-compare-image';
-import { MdArrowForwardIos } from "react-icons/md";
-import { MdArrowBackIosNew } from "react-icons/md";
+import { MdArrowForwardIos, MdArrowBackIos } from "react-icons/md";
 
 const initialImages = [
     'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_960_720.jpg',
@@ -15,12 +14,16 @@ const initialImages = [
     'https://cdn.pixabay.com/photo/2020/04/27/09/21/cat-5098930_960_720.jpg'
 ];
 
-const ImageSlider = () => {
+const ImageSlider = ({ images }) => {
     const [selectedImage, setSelectedImage] = useState(null);
     const [selectedIndex, setSelectedIndex] = useState(null);
     const [selectedImages, setSelectedImages] = useState([]);
-    const [unselectedImages, setUnselectedImages] = useState(initialImages);
+    const [unselectedImages, setUnselectedImages] = useState(initialImages || []);
+    const [isOpen, setIsOpen] = useState(false);
 
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+      };
     const handleImageClick = (image, index, isSelected) => {
         setSelectedImage(image);
         setSelectedIndex(index);
@@ -43,27 +46,19 @@ const ImageSlider = () => {
     };
 
     const handleNextImage = () => {
-        if (selectedImages.includes(selectedImage)) {
-            const nextIndex = (selectedIndex + 1) % selectedImages.length;
-            setSelectedImage(selectedImages[nextIndex]);
-            setSelectedIndex(nextIndex);
-        } else {
-            const nextIndex = (selectedIndex + 1) % unselectedImages.length;
-            setSelectedImage(unselectedImages[nextIndex]);
-            setSelectedIndex(nextIndex);
-        }
+        const imagesArray = selectedImages.includes(selectedImage) ? selectedImages : unselectedImages;
+        const currentIndex = imagesArray.indexOf(selectedImage);
+        const nextIndex = (currentIndex + 1) % imagesArray.length;
+        setSelectedImage(imagesArray[nextIndex]);
+        setSelectedIndex(nextIndex);
     };
 
     const handlePreviousImage = () => {
-        if (selectedImages.includes(selectedImage)) {
-            const prevIndex = (selectedIndex - 1 + selectedImages.length) % selectedImages.length;
-            setSelectedImage(selectedImages[prevIndex]);
-            setSelectedIndex(prevIndex);
-        } else {
-            const prevIndex = (selectedIndex - 1 + unselectedImages.length) % unselectedImages.length;
-            setSelectedImage(unselectedImages[prevIndex]);
-            setSelectedIndex(prevIndex);
-        }
+        const imagesArray = selectedImages.includes(selectedImage) ? selectedImages : unselectedImages;
+        const currentIndex = imagesArray.indexOf(selectedImage);
+        const prevIndex = (currentIndex - 1 + imagesArray.length) % imagesArray.length;
+        setSelectedImage(imagesArray[prevIndex]);
+        setSelectedIndex(prevIndex);
     };
 
     const handleModalClick = (e) => {
@@ -75,9 +70,84 @@ const ImageSlider = () => {
     return (
         <div className="p-4">
             <div className='mb-6'>
-                <h2 className="text-xl font-bold">All Images</h2>
-                <div className="flex overflow-x-scroll py-4 space-x-4">
-                    {unselectedImages.map((image, index) => (
+            <div className="flex justify-between items-center gap-2 pb-2 ">
+                      <h1 className="font-semibold text-lg ">Downloadable</h1>
+                      {/* --------------------------------------------------download--------------------------------------- */}
+                      <div className="">
+                        <div className="relative inline-block text-left">
+                          <div className="flex justify-center items-center gap-2">
+                         
+                             
+                          
+                            <button
+                              type="button"
+                              onClick={toggleDropdown}
+
+                              className="px-1.5 py-1    bg-gray-400 rounded-md text-white outline-none  shadow-lg  flex justify-center items-center gap-2 "
+
+                              id="options-menu"
+                              aria-expanded="true"
+                              aria-haspopup="true"
+                            >
+                              {/* <span className="">Download</span> */}
+                              {/* Dropdown arrow */}
+                              <svg
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth="2"
+                                  d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+                                />
+                              </svg>
+
+
+
+                            </button>
+                          </div>
+
+                          {/* Dropdown panel */}
+                          {isOpen && (
+                            <div
+                              className="origin-top-right absolute right-0 mt-1 z-[999] rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none "
+                              role="menu"
+
+                              aria-orientation="vertical"
+                              aria-labelledby="options-menu"
+                            >
+                              {/* Dropdown items */}
+                              <button
+                                // onClick={() => zipFileDownload("jpg")}
+                                className="block px-6 py-1 w-full text-[11px] text-gray-700 hover:bg-gray-200 hover:text-gray-900"
+                                role="menuitem"
+                              >
+                                JPG
+                              </button>
+                              <button
+                                // onClick={() => zipFileDownload("png")}
+                                className="block px-6 py-1 text-[11px] w-full text-gray-700 hover:bg-gray-200 hover:text-gray-900"
+                                role="menuitem"
+                              >
+                                PNG
+                              </button>
+                              <button
+                                // onClick={() => zipFileDownload("psd")}
+                                className="block px-6 py-1 text-[11px] w-full text-gray-700 hover:bg-gray-200 hover:text-gray-900"
+                                role="menuitem"
+                              >
+                                PSD
+                              </button>
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                <div className="flex overflow-x-auto py-2 space-x-4">
+                    {unselectedImages && unselectedImages.map((image, index) => (
                         <div key={index} className="relative flex-shrink-0">
                             <img
                                 src={image}
@@ -91,9 +161,9 @@ const ImageSlider = () => {
             </div>
 
             <div className="">
-                <h2 className="text-xl font-bold">Selected Images</h2>
+                <h2 className="text-xl font-bold">Select for rework</h2>
                 <div className="flex overflow-x-auto py-4 space-x-4">
-                    {selectedImages.map((image, index) => (
+                    {selectedImages && selectedImages.map((image, index) => (
                         <div key={index} className="relative flex-shrink-0">
                             <img
                                 src={image}
@@ -111,19 +181,16 @@ const ImageSlider = () => {
                     className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
                     onClick={handleModalClick}
                 >
-                    <div className="bg-white  rounded relative flex flex-col items-center">
-                        <div className='h-[400px] w-[400px]  rounded-lg shadow-lg '>
+                    <div className="bg-white rounded relative flex flex-col items-center">
+                        <div className='h-[300px] w-[330px]  rounded-lg shadow-lg '>
                             <div className='h-full flex flex-col justify-center'>
                                 <ReactCompareImage
                                     leftImage={selectedImage}
                                     rightImage={selectedImage}
-
                                 />
                             </div>
                         </div>
-                    
                         <div className="flex space-x-4 py-2">
-                          
                             <button
                                 className={`px-4 py-2 ${selectedImages.includes(selectedImage) ? 'bg-red-700' : 'bg-green-700'
                                     } text-white rounded`}
@@ -131,10 +198,9 @@ const ImageSlider = () => {
                             >
                                 {selectedImages.includes(selectedImage) ? 'Deselect' : 'Select for rework'}
                             </button>
-                           
                         </div>
-                        <button onClick={handlePreviousImage} className="absolute top-1/2 transform -translate-y-1/2 left-0  text-3xl text-[#003333] hover:text-[#006464] transition duration-500 z-[9]">< MdArrowBackIosNew/></button>
-                            <button onClick={handleNextImage} className="absolute top-1/2 transform -translate-y-1/2 right-0  text-3xl text-[#003333] hover:text-[#006464] transition duration-500 z-[9]">< MdArrowForwardIos/></button>
+                        <button onClick={handlePreviousImage} className="absolute top-1/2 transform -translate-y-1/2 left-0  text-3xl text-[#003333] hover:text-[#006464] transition duration-500 z-[9]"><MdArrowBackIos /></button>
+                        <button onClick={handleNextImage} className="absolute top-1/2 transform -translate-y-1/2 right-0  text-3xl text-[#003333] hover:text-[#006464] transition duration-500 z-[9]"><MdArrowForwardIos /></button>
                     </div>
                 </div>
             )}
